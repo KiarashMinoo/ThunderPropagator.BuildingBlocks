2de0cca22e3a239b457e1c887aaf736a5b9deb7e
Kiarash Minoo <yanis_1984@msn.com>
2024-11-02 10:44:00 +0330

    setting nuget packages

--- pack.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Clean and build in release
-dotnet restore
-dotnet clean
-dotnet build -c Release
-
-# Create all NuGet packages
-dotnet pack ./BuildingBlocks/RapidStreamer.BuildingBlocks.Infrastructure/RapidStreamer.BuildingBlocks.Infrastructure.csproj --no-build -c Release -o ./artifacts
