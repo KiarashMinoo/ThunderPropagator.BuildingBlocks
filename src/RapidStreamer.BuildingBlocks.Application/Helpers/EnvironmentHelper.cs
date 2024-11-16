e06158f526dd6213e4f69833f3ce31dec7d2ccc5
Kiarash Minoo <yanis_1984@msn.com>
2024-11-16 11:09:56 +0330

    add environment keys support

---
+++ src/RapidStreamer.BuildingBlocks.Application/Helpers/EnvironmentHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace RapidStreamer.BuildingBlocks.Application.Helpers;
+
+public static class EnvironmentHelper
+{
+    public static IEnumerable<string> GetEnvironmentKeys(this string str)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(str);
+        var index = 0;
+        while (true)
+        {
+            index = str.IndexOf('$', index);
+            if (index <= 0)
+                break;
+
+            var nextIndex = str.IndexOf('$', index + 1) + 1;
+            if (nextIndex <= 0)
+                break;
+
+            yield return str.Substring(index, nextIndex - index);
+
+            index = nextIndex;
+        }
+    }
+}
