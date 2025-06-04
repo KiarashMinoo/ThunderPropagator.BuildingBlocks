6f0136bc00134f678e9c4fdbd927807b6da69381
Ahmad(Kia) Minoo <ahmadminoo@gmail.com>
2025-06-04 15:16:17 +0330

    fix readYaml on yaml type convertors

--- src/RapidStreamer.BuildingBlocks.Application/Serializations/Yaml/YamlTypeConverter.cs
+++ src/RapidStreamer.BuildingBlocks.Application/Serializations/Yaml/YamlTypeConverter.cs
@@ -15,12 +15,6 @@ namespace RapidStreamer.BuildingBlocks.Application.Serializations.Yaml
 
     public abstract class BaseYamlTypeConverter
     {
-        protected readonly Type MappingStartType = typeof(MappingStart);
-        protected readonly Type MappingEndType = typeof(MappingEnd);
-
-        protected readonly Type SequenceStartType = typeof(SequenceStart);
-        protected readonly Type SequenceEndType = typeof(SequenceEnd);
-
         protected readonly char[] KeyCharactersThatRequireQuotes = [' ', '/', '\\', '~', ':', '$', '{', '}'];
 
         public abstract bool Accepts(Type type);
@@ -31,163 +25,124 @@ namespace RapidStreamer.BuildingBlocks.Application.Serializations.Yaml
             return converterType is not null ? (IYamlTypeConverter)Activator.CreateInstance(converterType)! : null;
         }
 
-        protected bool DoubleShift(IParser parser) => parser.MoveNext() && parser.MoveNext();
+        protected bool ShiftIf(Func<IParser, bool> check, IParser parser)
+        {
+            var result = check(parser);
+            if (result) parser.MoveNext();
+            return result;
+        }
+
+        protected bool IsMappingStart(IParser parser) => parser.Current is MappingStart;
+        protected bool IsMappingEnd(IParser parser) => parser.Current is MappingEnd;
+        protected bool IsSequenceStart(IParser parser) => parser.Current is SequenceStart;
+        protected bool IsSequenceEnd(IParser parser) => parser.Current is SequenceEnd;
+
+        protected bool IsMappingStartAndShift(IParser parser) => ShiftIf(IsMappingStart, parser);
+        protected bool IsMappingEndAndShift(IParser parser) => ShiftIf(IsMappingEnd, parser);
+        protected bool IsSequenceStartAndShift(IParser parser) => ShiftIf(IsSequenceStart, parser);
+        protected bool IsSequenceEndAndShift(IParser parser) => ShiftIf(IsSequenceEnd, parser);
 
         protected void WriteMappingStart(IEmitter emitter) => emitter.Emit(new MappingStart(AnchorName.Empty, TagName.Empty, isImplicit: true, MappingStyle.Block));
-        protected bool IsMappingStart(IParser parser) => parser.Current?.GetType() == MappingStartType;
-
         protected void WriteMappingEnd(IEmitter emitter) => emitter.Emit(new MappingEnd());
-        protected bool IsMappingEnd(IParser parser) => parser.Current?.GetType() == MappingEndType;
-
-        protected bool IsMappingEndAndShift(IParser parser)
-        {
-            var rtn = IsMappingEnd(parser);
-
-            if (rtn)
-                parser.MoveNext();
-
-            return rtn;
-        }
-
         protected void WriteSequenceStart(IEmitter emitter) => emitter.Emit(new SequenceStart(AnchorName.Empty, TagName.Empty, isImplicit: true, SequenceStyle.Block));
-        protected bool IsSequenceStart(IParser parser) => parser.Current?.GetType() == SequenceStartType;
-
-        protected bool IsSequenceStartAndShift(IParser parser)
-        {
-            var rtn = IsSequenceStart(parser);
-
-            parser.MoveNext();
-
-            return rtn;
-        }
-
         protected void WriteSequenceEnd(IEmitter emitter) => emitter.Emit(new SequenceEnd());
-        protected bool IsSequenceEnd(IParser parser) => parser.Current?.GetType() == SequenceEndType;
 
-        protected bool IsSequenceEndAndShift(IParser parser)
-        {
-            var rtn = IsSequenceEnd(parser);
 
-            if (rtn)
-                parser.MoveNext();
-
-            return rtn;
-        }
-
-        //Serialization
         protected void Serialize(IEmitter emitter, object? value, Type type, ObjectSerializer serializer)
             => GetYamlTypeConverter(value?.GetType() ?? type)?.WriteYaml(emitter, value, type, serializer);
-
-        protected object? Deserialize(IParser parser, Type type, ObjectDeserializer rootDeserializer) => GetYamlTypeConverter(type)?.ReadYaml(parser, type, rootDeserializer);
-
-        protected object? DeserializeAndShift(IParser parser, Type type, ObjectDeserializer rootDeserializer)
-        {
-            var rtn = Deserialize(parser, type, rootDeserializer);
-
-            parser.MoveNext();
-
-            return rtn;
-        }
 
         protected void Serialize<TAny>(IEmitter emitter, TAny? value, ObjectSerializer serializer)
             => (GetYamlTypeConverter(value?.GetType() ?? typeof(TAny)) as IYamlTypeConverter<TAny>)?.WriteYaml(emitter, value, typeof(TAny), serializer);
 
+        protected object? Deserialize(IParser parser, Type type, ObjectDeserializer rootDeserializer)
+            => GetYamlTypeConverter(type)?.ReadYaml(parser, type, rootDeserializer);
+
         protected TAny? Deserialize<TAny>(IParser parser, ObjectDeserializer rootDeserializer)
             => GetYamlTypeConverter(typeof(TAny)) is IYamlTypeConverter<TAny> converter ? converter.ReadYaml(parser, rootDeserializer) : default;
 
-        protected TAny? DeserializeAndShift<TAny>(IParser parser, ObjectDeserializer rootDeserializer)
+        protected object? DeserializeAndShift(IParser parser, Type type, ObjectDeserializer rootDeserializer)
         {
-            var rtn = Deserialize<TAny>(parser, rootDeserializer);
-
+            var result = Deserialize(parser, type, rootDeserializer);
             parser.MoveNext();
-
-            return rtn;
+            return result;
         }
 
-        //Key Value
+        protected TAny? DeserializeAndShift<TAny>(IParser parser, ObjectDeserializer rootDeserializer)
+        {
+            var result = Deserialize<TAny>(parser, rootDeserializer);
+            parser.MoveNext();
+            return result;
+        }
+
+
         protected void WriteKey(IEmitter emitter, string key)
         {
-            var keyScalar = key.IndexOfAny(KeyCharactersThatRequireQuotes) >= 0
-                ? new Scalar(AnchorName.Empty, TagName.Empty, key, ScalarStyle.DoubleQuoted, isPlainImplicit: false, isQuotedImplicit: true)
-                : new Scalar(AnchorName.Empty, TagName.Empty, key, ScalarStyle.Plain, isPlainImplicit: true, isQuotedImplicit: false);
-
+            var style = key.IndexOfAny(KeyCharactersThatRequireQuotes) >= 0 ? ScalarStyle.DoubleQuoted : ScalarStyle.Plain;
+            var keyScalar = new Scalar(null, null, key, style, isPlainImplicit: style == ScalarStyle.Plain, isQuotedImplicit: style != ScalarStyle.Plain);
             emitter.Emit(keyScalar);
         }
 
         protected string ReadKey(IParser parser)
         {
             var key = ReadValue(parser);
-
             if (string.IsNullOrWhiteSpace(key))
                 throw new InvalidDataException("Invalid YAML content.");
-
             parser.MoveNext();
-
             return key;
         }
 
         protected void WriteValue(IEmitter emitter, string value)
-        {
-            emitter.Emit(new Scalar(AnchorName.Empty, TagName.Empty, value, ScalarStyle.DoubleQuoted, isPlainImplicit: false, isQuotedImplicit: true));
-        }
+            => emitter.Emit(new Scalar(null, null, value, ScalarStyle.DoubleQuoted, false, true));
 
         protected string? ReadValue(IParser parser) => (parser.Current as Scalar)?.Value;
 
         protected string? ReadValueAndShift(IParser parser)
         {
-            var rtn = ReadValue(parser);
-
+            var value = ReadValue(parser);
             parser.MoveNext();
-
-            return rtn;
-        }
-
-        protected void WriteKeyValue(IEmitter emitter, string key, string value)
-        {
-            if (string.IsNullOrWhiteSpace(key) || string.IsNullOrWhiteSpace(value))
-                return;
-
-            WriteKey(emitter, key);
-            WriteValue(emitter, value);
+            return value;
         }
 
         //Enum
+        protected void WriteKeyValue(IEmitter emitter, string key, string value)
+        {
+            if (!string.IsNullOrWhiteSpace(key) && !string.IsNullOrWhiteSpace(value))
+            {
+                WriteKey(emitter, key);
+                WriteValue(emitter, value);
+            }
+        }
+
         protected void WriteEnum(IEmitter emitter, string key, Enum? value)
         {
             if (value is not null)
-                WriteKeyValue(emitter, key, value.ToString());
+                WriteKeyValue(emitter, key, value.ToString()!);
         }
 
         protected Enum? ReadEnum(IParser parser, Type type)
         {
-            var scalar = parser.Current as Scalar;
-            return !string.IsNullOrWhiteSpace(scalar?.Value) && Enum.TryParse(type, scalar.Value, true, out var @enum) ? @enum as Enum : null;
+            var value = ReadValue(parser);
+            return Enum.TryParse(type, value, true, out var result) ? result as Enum : null;
         }
 
         protected Enum? ReadEnumAndShift(IParser parser, Type type)
         {
-            var rtn = ReadEnum(parser, type);
-
+            var result = ReadEnum(parser, type);
             parser.MoveNext();
-
-            return rtn;
+            return result;
         }
 
-        protected TEnum ReadEnum<TEnum>(IParser parser)
-            where TEnum : struct
+        protected TEnum ReadEnum<TEnum>(IParser parser) where TEnum : struct
         {
-            var scalar = parser.Current as Scalar;
-            return !string.IsNullOrWhiteSpace(scalar?.Value) && Enum.TryParse<TEnum>(scalar.Value, true, out var @enum) ? @enum : default;
+            var value = ReadValue(parser);
+            return Enum.TryParse(value, true, out TEnum result) ? result : default;
         }
 
-        protected TEnum ReadEnumAndShift<TEnum>(IParser parser)
-            where TEnum : struct
+        protected TEnum ReadEnumAndShift<TEnum>(IParser parser) where TEnum : struct
         {
-            var rtn = ReadEnum<TEnum>(parser);
-
+            var result = ReadEnum<TEnum>(parser);
             parser.MoveNext();
-
-            return rtn;
+            return result;
         }
 
         //Boolean
@@ -199,115 +154,43 @@ namespace RapidStreamer.BuildingBlocks.Application.Serializations.Yaml
 
         protected bool? ReadBoolean(IParser parser)
         {
-            var scalar = parser.Current as Scalar;
-
-            return string.IsNullOrWhiteSpace(scalar?.Value)
-                ? null
-                : string.Equals(scalar.Value, "1") ||
-                  string.Equals(scalar.Value, "true", StringComparison.InvariantCultureIgnoreCase) ||
-                  string.Equals(scalar.Value, "yes", StringComparison.InvariantCultureIgnoreCase);
+            return (parser.Current as Scalar)?.Value?.ToLowerInvariant() switch
+            {
+                "1" or "true" or "yes" => true,
+                "0" or "false" or "no" => false,
+                _ => null
+            };
         }
 
         protected bool? ReadBooleanAndShift(IParser parser)
         {
-            var rtn = ReadBoolean(parser);
-
+            var result = ReadBoolean(parser);
             parser.MoveNext();
-
-            return rtn;
+            return result;
         }
 
         //Number
-        protected void WriteNumber<TNumber>(IEmitter emitter, string key, TNumber? value)
-            where TNumber : INumber<TNumber>
+        protected void WriteNumber<TNumber>(IEmitter emitter, string key, TNumber? value) where TNumber : INumber<TNumber>
         {
             if (value is not null)
                 WriteKeyValue(emitter, key, value.ToString()!);
         }
 
-        protected TNumber? ReadNumber<TNumber>(IParser parser)
-            where TNumber : INumber<TNumber>
+        protected TNumber? ReadNumber<TNumber>(IParser parser) where TNumber : INumber<TNumber>
         {
-            var rtn = ReadValue(parser);
-
-            if (!string.IsNullOrEmpty(rtn) && TNumber.TryParse(rtn, CultureInfo.CurrentCulture, out var result))
-                return result;
-
-            return default;
+            var value = ReadValue(parser);
+            return TNumber.TryParse(value, CultureInfo.CurrentCulture, out var result) ? result : default;
         }
 
-        protected TNumber? ReadNumberAndShift<TNumber>(IParser parser)
-            where TNumber : INumber<TNumber>
+        protected TNumber? ReadNumberAndShift<TNumber>(IParser parser) where TNumber : INumber<TNumber>
         {
-            var rtn = ReadNumber<TNumber>(parser);
-
+            var result = ReadNumber<TNumber>(parser);
             parser.MoveNext();
-
-            return rtn;
-        }
-
-
-        //Throw Part
-        protected bool IsMappingStartAndShift(IParser parser)
-        {
-            var rtn = IsMappingStart(parser);
-
-            parser.MoveNext();
-
-            return rtn;
-        }
-
-        protected void ThrowIfIsNotMappingEnd(IParser parser)
-        {
-            if (!IsMappingEnd(parser))
-                throw new InvalidDataException("Invalid YAML content.");
-        }
-
-        protected void ThrowIfIsNotSequenceEnd(IParser parser)
-        {
-            if (!IsSequenceEnd(parser))
-                throw new InvalidDataException("Invalid YAML content.");
-        }
-
-        protected void ThrowIfIsNotMappingStart(IParser parser)
-        {
-            if (!IsMappingStart(parser))
-                throw new InvalidDataException("Invalid YAML content.");
-        }
-
-        protected void ThrowIfIsNotSequenceStart(IParser parser)
-        {
-            if (!IsSequenceStart(parser))
-                throw new InvalidDataException("Invalid YAML content.");
-        }
-
-        protected void ThrowIfIsNotMappingEndAndShift(IParser parser)
-        {
-            if (!IsMappingEndAndShift(parser))
-                throw new InvalidDataException("Invalid YAML content.");
-        }
-
-        protected void ThrowIfIsNotSequenceEndAndShift(IParser parser)
-        {
-            if (!IsSequenceEndAndShift(parser))
-                throw new InvalidDataException("Invalid YAML content.");
-        }
-
-        protected void ThrowIfIsNotMappingStartAndShift(IParser parser)
-        {
-            if (!IsMappingStartAndShift(parser))
-                throw new InvalidDataException("Invalid YAML content.");
-        }
-
-        protected void ThrowIfIsNotSequenceStartAndShift(IParser parser)
-        {
-            if (!IsSequenceStartAndShift(parser))
-                throw new InvalidDataException("Invalid YAML content.");
+            return result;
         }
     }
 
-    public abstract class YamlTypeConverter : BaseYamlTypeConverter,
-        IYamlTypeConverter
+    public abstract class YamlTypeConverter : BaseYamlTypeConverter, IYamlTypeConverter
     {
         void IYamlTypeConverter.WriteYaml(IEmitter emitter, object? value, Type type, ObjectSerializer serializer)
         {
@@ -319,39 +202,42 @@ namespace RapidStreamer.BuildingBlocks.Application.Serializations.Yaml
 
         object? IYamlTypeConverter.ReadYaml(IParser parser, Type type, ObjectDeserializer rootDeserializer)
         {
-            ThrowIfIsNotMappingStartAndShift(parser);
+            if (!IsMappingStartAndShift(parser) && !IsSequenceStartAndShift(parser))
+                throw new InvalidDataException("Invalid YAML content.");
             return ReadYamlInternal(parser, type, rootDeserializer);
         }
 
         protected abstract object? ReadYamlInternal(IParser parser, Type type, ObjectDeserializer rootDeserializer);
     }
 
-    public abstract class YamlTypeConverter<T> : BaseYamlTypeConverter,
-        IYamlTypeConverter<T>
+    public abstract class YamlTypeConverter<T> : BaseYamlTypeConverter, IYamlTypeConverter<T>
     {
         public override bool Accepts(Type type) => type == typeof(T);
 
         void IYamlTypeConverter.WriteYaml(IEmitter emitter, object? value, Type type, ObjectSerializer serializer)
         {
-            if (value is T instance)
-                WriteYamlInternal(emitter, instance, type, serializer);
+            if (value is T typed)
+                WriteYamlInternal(emitter, typed, type, serializer);
         }
 
-        void IYamlTypeConverter<T>.WriteYaml(IEmitter emitter, T? value, ObjectSerializer serializer) => WriteYamlInternal(emitter, value, typeof(T), serializer);
-        protected abstract void WriteYamlInternal(IEmitter emitter, T? value, Type type, ObjectSerializer serializer);
+        void IYamlTypeConverter<T>.WriteYaml(IEmitter emitter, T? value, ObjectSerializer serializer)
+            => WriteYamlInternal(emitter, value, typeof(T), serializer);
 
         object? IYamlTypeConverter.ReadYaml(IParser parser, Type type, ObjectDeserializer rootDeserializer)
         {
-            ThrowIfIsNotMappingStartAndShift(parser);
+            if (!IsMappingStartAndShift(parser) && !IsSequenceStartAndShift(parser))
+                throw new InvalidDataException("Invalid YAML content.");
             return ReadYamlInternal(parser, type, rootDeserializer);
         }
 
         T? IYamlTypeConverter<T>.ReadYaml(IParser parser, ObjectDeserializer rootDeserializer)
         {
-            ThrowIfIsNotMappingStartAndShift(parser);
+            if (!IsMappingStartAndShift(parser) && !IsSequenceStartAndShift(parser))
+                throw new InvalidDataException("Invalid YAML content.");
             return ReadYamlInternal(parser, typeof(T), rootDeserializer);
         }
 
+        protected abstract void WriteYamlInternal(IEmitter emitter, T? value, Type type, ObjectSerializer serializer);
         protected abstract T? ReadYamlInternal(IParser parser, Type type, ObjectDeserializer rootDeserializer);
     }
 }
