8f7f08819b8ab2a2e63c9b23a245306fe27374c9
Kiarash Minoo <53106285+KiarashMinoo@users.noreply.github.com>
2025-12-28 13:35:20 +0330

    fix: update package name filter to match ThunderPropagator.BuildingBlocks

--- .github/scripts/Delete-GitHubPackages.ps1
+++ .github/scripts/Delete-GitHubPackages.ps1
@@ -8,7 +8,7 @@ param(
 
     # Package name filter (supports wildcards: *, ?)
     # Use '*' to clean ALL user packages
-    [string]$PackageNameFilter = 'RapidStreamer*',
+    [string]$PackageNameFilter = 'ThunderPropagator.BuildingBlocks*',
     [string[]]$PackageTypes = @('nuget'),
     [int]$PageSize = 1000,
     [switch]$SinglePass,
