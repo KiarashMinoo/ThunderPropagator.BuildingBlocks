65a033cd70b32665d0da106b3252cbb5a40049ea
Kiarash Minoo <53106285+KiarashMinoo@users.noreply.github.com>
2025-12-28 13:26:40 +0330

    Add benchmark log for CollectionHelperBenchmark test suite

--- src/ThunderPropagator.BuildingBlocks.Infrastructure/SystemResourceMonitor/Metrics/Disk/DiskSpeedMetricsClient.cs
+++ src/ThunderPropagator.BuildingBlocks.Infrastructure/SystemResourceMonitor/Metrics/Disk/DiskSpeedMetricsClient.cs
@@ -1,4 +1,4 @@
-?using System.Diagnostics;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace ThunderPropagator.BuildingBlocks.Infrastructure.SystemResourceMonitor.Metrics.Disk;
